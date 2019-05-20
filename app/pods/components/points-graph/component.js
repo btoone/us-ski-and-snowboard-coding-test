132da3cff3eddda249325b499cccaccb4391016f
Brandon Toone <brandon@rdlk.co>
2019-05-20 15:40:15 -0600

    Add charting component
    Adds a charting component that uses the Chart.js library and graphs
    datasets onto a canvas element.

--- app/pods/components/points-graph/component.js
+++ app/pods/components/points-graph/component.js
@@ -24,20 +24,79 @@ import $ from 'jquery';
 export default Component.extend({
 	classNames: ['points-graph'], // Class styling in /app/styles/components/points-graph.scss
 
+	/**
+	 * Ember component hook
+	 *  
+	 **/
 
-	// Ember hook called when the component is inserted into the DOM
-	didInsertElement() {
-		this.draw();
+	didReceiveAttrs() {
+		this._super(...arguments);
+
+		let sets = this.userData.sets;
+		let points = this.userData.points;
+		let options = this.createChartOptions(sets);
+
+		this.datasets = this.addDataAttribute(options, points);
 	},
 
+	/**
+	 * Ember hook called when the component is inserted into the DOM
+	 **/
+
+	didInsertElement() {
+		this.draw(this.datasets);
+	},
+
+	/**
+	 * Returns an array of chart options for each dataSet
+	 * 
+	 * @method createChartOptions
+	 * @param {Array} dataSets A collection of data sets
+	 * @returns {Array} A collection of Chart.js dataset objects
+	 **/
+
+	createChartOptions(dataSets) {
+		let results = new Array();
+	
+		dataSets.forEach((item, index) => {
+		let options = new Object();
+	
+		options.backgroundColor = item.color;
+		options.borderColor     = item.color;
+		options.fill            = false;
+		options.label           = item.name;
+
+		results.push(options);
+	});
+
+	return results;
+},
+
+	/**
+	 * Add filtered and sorted dataPoints to the associated dataset
+	 *
+	 * @method addDataAttribute
+	 * @param {Array} chartOptions A collection of Chart.js dataset objects
+	 * @param {Array} dataPoints A collection of data points for multiple users
+	 * @returns {Array} The mutated collection of Chart.js dataset objects
+	 **/
+
+	addDataAttribute(chartOptions, dataPoints) {
+		chartOptions.forEach((item, index) => {
+			item.data = dataPoints.filterBy('name', item.label).sortBy('x');
+		});
+
+		return chartOptions;
+	},
 
 	/**
 	 * Draw data on canvas
 	 *
 	 * @method draw
+	 * @param {Array} datasets A collection of Chart.js dataset objects
 	 **/
 
-	draw() {
+	draw(datasets) {
 		let $canvas = this.$('canvas'); // jQuery reference to the canvas element in template.hbs
 
 		/**
@@ -51,6 +110,24 @@ export default Component.extend({
 		 *   - You may use any library to draw on the canvas or call the canvas functions directly.
 		 **/
 
+		// Setup Chart.js configuration
+		let config = {
+			type: 'line',
+			data: {
+				datasets: datasets
+			},
+			options: {
+				scales: {
+					xAxes: [{
+						type: 'linear',
+						position: 'bottom'
+					}]
+				}
+			}
+		};
+    
+		// Render the chart
+		new Chart($canvas, config);
 	}
 
 
